38dd5fcae5cb65e528fce8299412a4e404995edf
Martin Zlámal <mrtnzlml@gmail.com>
2016-10-05 14:40:46 +0200

    Move config directory to the project root

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -8,9 +8,9 @@ $configurator = new Nette\Configurator();
 //$configurator->setDebugMode('23.75.345.200'); // enable for your remote IP
 $configurator->enableDebugger(__DIR__ . '/../log');
 $configurator->setTempDirectory(__DIR__ . '/../temp');
-$configurator->addConfig(__DIR__ . '/config/config.neon');
+$configurator->addConfig(__DIR__ . '/../config/config.neon');
 
-$localConfig = __DIR__ . '/config/config.local.neon';
+$localConfig = __DIR__ . '/../config/config.local.neon';
 if (file_exists($localConfig)) {
 	$configurator->addConfig($localConfig);
 }
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,6 +3,6 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 Testbench\Bootstrap::setup(__DIR__ . '/_temp', function (\Nette\Configurator $configurator) {
-	$configurator->addConfig(__DIR__ . '/../app/config/config.neon');
-	// $configurator->addConfig(__DIR__ . '/../app/config/config.local.neon');
+	$configurator->addConfig(__DIR__ . '/../config/config.neon');
+	$configurator->addConfig(__DIR__ . '/../config/config.local.neon');
 });
