d84278aa2baf5eb3571f9672e07f11b5eca01b5c
Martin Zlámal <mrtnzlml@gmail.com>
2016-10-14 22:43:54 +0200

    Add bin/console

--- www/index.php
+++ www/index.php
@@ -2,5 +2,9 @@
 
 //http://php.net/manual/en/functions.arguments.php#functions.arguments.type-declaration.strict
 
+if (PHP_SAPI === 'cli') {
+	die("\e[37;41m To run application in CLI mode use 'bin/console' instead. \e[0m\n");
+}
+
 $container = require __DIR__ . '/../app/bootstrap.php';
 $container->getByType(Nette\Application\Application::class)->run();
