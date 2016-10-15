d91ccc916647354b43ef6f6b7899ff6cc933294f
Martin Zlámal <mrtnzlml@gmail.com>
2016-10-15 11:39:34 +0200

    CLI: enable debug mode

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -5,6 +5,17 @@ $classLoader = require __DIR__ . '/../vendor/autoload.php';
 
 $configurator = new Nette\Configurator();
 
+if (PHP_SAPI === 'cli') {
+	$input = new \Symfony\Component\Console\Input\ArgvInput;
+	$env = $input->getParameterOption(['--env', '-e'], getenv('NETTE_ENV') ?: 'dev');
+	$debug = getenv('NETTE_DEBUG') !== '0' && !$input->hasParameterOption(['--no-debug', '']) && $env !== 'prod';
+
+	if ($debug) {
+		\Symfony\Component\Debug\Debug::enable();
+		$configurator->setDebugMode(TRUE);
+	}
+}
+
 //$configurator->setDebugMode('23.75.345.200'); // enable for your remote IP
 $configurator->enableDebugger(__DIR__ . '/../log');
 $configurator->setTempDirectory(__DIR__ . '/../temp');
