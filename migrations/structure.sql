8f7976e6437f0ddead209098151b6d99212f932f
Martin Zlámal <mrtnzlml@gmail.com>
2016-11-05 18:59:00 +0100

    Add SQL structure

---
+++ migrations/structure.sql
@@ -0,0 +1,19 @@
+CREATE TABLE public.data_sources_records (
+	id UUID PRIMARY KEY NOT NULL,
+	data_source_id UUID NOT NULL,
+	data JSONB NOT NULL
+);
+COMMENT ON COLUMN data_sources_records.id IS '(DC2Type:uuid)';
+COMMENT ON COLUMN data_sources_records.data_source_id IS '(DC2Type:uuid)';
+
+
+CREATE TABLE public.data_sources (
+	id UUID PRIMARY KEY NOT NULL,
+	device_name VARCHAR(255) NOT NULL
+);
+COMMENT ON COLUMN data_sources.id IS '(DC2Type:uuid)';
+
+
+CREATE TABLE public.database_structure_migrations (
+	version VARCHAR(255) PRIMARY KEY NOT NULL
+);
