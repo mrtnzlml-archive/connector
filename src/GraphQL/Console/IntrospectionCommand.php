f50a4d8382ee62c9cdf55a416e36d49b0dee5824
Martin Zlámal <mrtnzlml@gmail.com>
2016-11-18 16:11:31 +0100

    GraphQL: add introspection command

---
+++ src/GraphQL/Console/IntrospectionCommand.php
@@ -0,0 +1,75 @@
+<?php declare(strict_types = 1);
+
+namespace Adeira\Connector\GraphQL\Console;
+
+use Adeira\Connector\GraphQL\SchemaFactory;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class IntrospectionCommand extends \Symfony\Component\Console\Command\Command
+{
+
+	/** @var \Adeira\Connector\GraphQL\SchemaFactory */
+	private $schemaFactory;
+
+	private $internalTypes = [
+		'String',
+		'Int',
+		'__Schema',
+		'__Type',
+		'__TypeKind',
+		'Boolean',
+		'__Field',
+		'__InputValue',
+		'__EnumValue',
+		'__Directive',
+		'__DirectiveLocation',
+		'ID',
+		'Float',
+	];
+
+	public function injectDependencies(SchemaFactory $schemaFactory)
+	{
+		$this->schemaFactory = $schemaFactory;
+	}
+
+	protected function configure()
+	{
+		$this->setName('app:graphql:introspection')->setDescription('Introspect GraphQL schema.');
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		$schema = $this->schemaFactory->build();
+
+		/** @var \GraphQL\Type\Definition\ObjectType $objectType */
+		foreach ($schema->getTypeMap() as $typeName => $objectType) {
+			// skip internal types
+			if (in_array($typeName, $this->internalTypes)) {
+				continue;
+			}
+
+			$this->printType($input, $output, $objectType);
+		}
+	}
+
+	private function printType(
+		InputInterface $input,
+		OutputInterface $output,
+		\GraphQL\Type\Definition\ObjectType $objectType
+	) {
+		$output->writeln("<fg=blue;options=bold>type $objectType {</>");
+		foreach ($objectType->getFields() as $name => $fieldDefinition) {
+			$arguments = [];
+			/** @var \GraphQL\Type\Definition\FieldArgument $arg */
+			foreach ($fieldDefinition->args as $arg) {
+				$arguments[] = $arg->name . ': ' . (string)$arg->getType();
+			}
+			$arguments = $arguments ? '(' . implode(', ', $arguments) . ')' : '';
+			$output->writeln("  $name<fg=yellow>$arguments</>: <fg=green>{$fieldDefinition->getType()}</>");
+		}
+		$output->writeln('<fg=blue;options=bold>}</>');
+		$output->writeln('');
+	}
+
+}
