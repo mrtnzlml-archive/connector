e988c97e6300ce08f995adc3f94fcd18ed01743b
Martin Zlámal <mrtnzlml@gmail.com>
2017-01-15 10:17:41 +0100

    GraphQL: introduce adeira:graphql:query command

---
+++ extensions/GraphQL/Console/QueryCommand.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types = 1);
+
+namespace Adeira\Connector\GraphQL\Console;
+
+use Adeira\Connector\GraphQL\SchemaFactory;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class QueryCommand extends \Adeira\Connector\Symfony\Console\Command
+{
+
+	/** @var \Adeira\Connector\GraphQL\SchemaFactory */
+	private $schemaFactory;
+
+	public function __construct(SchemaFactory $schemaFactory)
+	{
+		parent::__construct();
+		$this->schemaFactory = $schemaFactory;
+	}
+
+	protected function configure()
+	{
+		$this->setName('graphql:query');
+		$this->setDescription('Query GraphQL API via command line.');
+		$this->addArgument('requestString', InputArgument::REQUIRED);
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		$graphResponse = \GraphQL\GraphQL::execute(
+			$this->schemaFactory->build(),
+			$input->getArgument('requestString')
+		);
+		$style = new SymfonyStyle($input, $output);
+		$style->block(json_encode($graphResponse, JSON_PRETTY_PRINT));
+	}
+
+}
