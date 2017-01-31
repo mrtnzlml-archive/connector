03554fe27223a31e8fdf0e6f66074ed07d68c183
Martin Zlámal <mrtnzlml@gmail.com>
2017-01-31 09:25:24 +0100

    Migrations: add dummy test user

---
+++ migrations/dummy-data/2017-01-31-091915-test-user.sql
@@ -0,0 +1,5 @@
+INSERT INTO user_accounts VALUES (
+		'00000000-0000-0000-0000-000000000001', -- UUID
+		'$2y$10$8nJLFVEGIFRnSlZ7yb5vjukQ/tJ/V4Yupa3OzNLoxOtCtp4fVekbe', -- password:test
+		'test' -- username:test
+);
