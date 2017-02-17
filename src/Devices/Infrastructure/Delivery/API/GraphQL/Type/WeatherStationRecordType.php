98d2ced320cb373f8a2e1daa6eb7bf9eb878f0f7
Martin Zlámal <mrtnzlml@gmail.com>
2017-02-17 10:31:02 +0100

    GraphQL: expose temperatures in WeatherStationRecord

--- src/Devices/Infrastructure/Delivery/API/GraphQL/Type/WeatherStationRecordType.php
+++ src/Devices/Infrastructure/Delivery/API/GraphQL/Type/WeatherStationRecordType.php
@@ -6,6 +6,7 @@ use Adeira\Connector\Devices\DomainModel\WeatherStation\WeatherStationRecord;
 use Adeira\Connector\GraphQL\Context;
 use Adeira\Connector\GraphQL\Structure\Field;
 use function Adeira\Connector\GraphQL\id;
+use function Adeira\Connector\GraphQL\nullableFloat;
 
 final class WeatherStationRecordType extends \Adeira\Connector\GraphQL\Structure\Type
 {
@@ -24,6 +25,9 @@ final class WeatherStationRecordType extends \Adeira\Connector\GraphQL\Structure
 	{
 		return [
 			$this->idFieldDefinition(),
+
+			$this->indoorTemperatureFieldDefinition(),
+			$this->outdoorTemperatureFieldDefinition(),
 		];
 	}
 
@@ -32,6 +36,24 @@ final class WeatherStationRecordType extends \Adeira\Connector\GraphQL\Structure
 		$field = new Field('id', 'ID of the weather station record', id());
 		$field->setResolveFunction(function (WeatherStationRecord $wsr, $args, Context $context) {
 			return $wsr->id();
+		});
+		return $field;
+	}
+
+	private function indoorTemperatureFieldDefinition()
+	{
+		$field = new Field('indoorTemperature', 'Indoor temperature', nullableFloat());
+		$field->setResolveFunction(function (WeatherStationRecord $wsr, $args, Context $context) {
+			return $wsr->temperature()->indoor();
+		});
+		return $field;
+	}
+
+	private function outdoorTemperatureFieldDefinition()
+	{
+		$field = new Field('outdoorTemperature', 'Outdoor temperature', nullableFloat());
+		$field->setResolveFunction(function (WeatherStationRecord $wsr, $args, Context $context) {
+			return $wsr->temperature()->outdoor();
 		});
 		return $field;
 	}
