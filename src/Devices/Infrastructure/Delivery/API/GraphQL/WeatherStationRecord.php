d3bf271bfeeec373d427637da1c2e4a8b12848cd
Martin Zlámal <mrtnzlml@gmail.com>
2017-02-20 09:48:50 +0100

    GraphQL WS record: expose pressures

--- src/Devices/Infrastructure/Delivery/API/GraphQL/WeatherStationRecord.php
+++ src/Devices/Infrastructure/Delivery/API/GraphQL/WeatherStationRecord.php
@@ -3,22 +3,31 @@
 namespace Adeira\Connector\Devices\Infrastructure\Delivery\API\GraphQL;
 
 use Adeira\Connector\Devices\DomainModel\WeatherStation\WeatherStationRecord as Record;
-use Adeira\Connector\GraphQL\Context;
 
 final class WeatherStationRecord
 {
 
-	public function id(Record $wsr, $args, Context $context)
+	public function id(Record $wsr)
 	{
 		return $wsr->id();
 	}
 
-	public function indoorTemperature(Record $wsr, $args, Context $context)
+	public function absolutePressure(Record $wsr): ?float
+	{
+		return $wsr->pressure()->absolute();
+	}
+
+	public function relativePressure(Record $wsr): ?float
+	{
+		return $wsr->pressure()->relative();
+	}
+
+	public function indoorTemperature(Record $wsr): ?float
 	{
 		return $wsr->temperature()->indoor();
 	}
 
-	public function outdoorTemperature(Record $wsr, $args, Context $context)
+	public function outdoorTemperature(Record $wsr): ?float
 	{
 		return $wsr->temperature()->outdoor();
 	}
