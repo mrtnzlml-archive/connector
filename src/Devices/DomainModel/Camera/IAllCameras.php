9edf467f1379d3b8b6744e04f36f8c9119d28eb5
Martin Zlámal <mrtnzlml@gmail.com>
2017-02-23 16:17:41 +0100

    Create IAllCameras with DoctrineAllCameras implementation

---
+++ src/Devices/DomainModel/Camera/IAllCameras.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types = 1);
+
+namespace Adeira\Connector\Devices\DomainModel\Camera;
+
+use Adeira\Connector\Authentication\DomainModel\Owner\Owner;
+use Adeira\Connector\Common\DomainModel\Stub;
+
+interface IAllCameras
+{
+
+	/**
+	 * Returns Stub of all cameras belonging to the Owner.
+	 */
+	public function belongingTo(Owner $owner): Stub;
+
+	/**
+	 * Returns next available Camera identifier.
+	 */
+	public function nextIdentity(): CameraId;
+
+}
---
+++ src/Devices/Infrastructure/Persistence/Doctrine/DoctrineAllCameras.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types = 1);
+
+namespace Adeira\Connector\Devices\Infrastructure\Persistence\Doctrine;
+
+use Adeira\Connector\Authentication\DomainModel\Owner\Owner;
+use Adeira\Connector\Common\DomainModel\Stub;
+use Adeira\Connector\Devices\DomainModel\Camera\{
+	Camera, CameraId, IAllCameras
+};
+use Doctrine\ORM;
+
+/**
+ * Do not call flush() here! Flushing and dealing with transactions is delegated to the Application Service.
+ * All behavior should still follow the Repositories’ collection characteristics.
+ */
+final class DoctrineAllCameras implements IAllCameras
+{
+
+	/**
+	 * @var \Doctrine\ORM\EntityManagerInterface
+	 */
+	private $em;
+
+	public function __construct(ORM\EntityManagerInterface $em)
+	{
+		$this->em = $em;
+	}
+
+	public function belongingTo(Owner $owner): Stub
+	{
+		$qb = $this->em->createQueryBuilder();
+		$qb->select($dqlAlias = 'cam')->from(Camera::class, $dqlAlias);
+		$qb->andWhere('cam.ownerId = :ownerId')->setParameter(':ownerId', $owner->id()->toString());
+
+		return Stub::wrap($qb);
+	}
+
+	public function nextIdentity(): CameraId
+	{
+		return CameraId::create();
+	}
+
+}
