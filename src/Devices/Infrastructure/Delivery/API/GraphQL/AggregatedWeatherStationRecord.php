c56ee4a7693bab740480527fa944689ee1fc728f
Martin Zlámal <mrtnzlml@gmail.com>
2017-03-12 14:29:36 +0100

    API: fix WeatherStationRecordsConnection return node type

---
+++ src/Devices/Infrastructure/Delivery/API/GraphQL/AggregatedWeatherStationRecord.php
@@ -0,0 +1,60 @@
+<?php declare(strict_types = 1);
+
+namespace Adeira\Connector\Devices\Infrastructure\Delivery\API\GraphQL;
+
+use Adeira\Connector\Devices\DomainModel\WeatherStation\AggregatedWeatherStationRecord as AggRecord;
+
+final class AggregatedWeatherStationRecord
+{
+
+	public function aggregatedDate(AggRecord $wsr): \DateTimeImmutable
+	{
+		return $wsr->aggregatedDate();
+	}
+
+	public function absolutePressure(AggRecord $wsr, array $args): ?float
+	{
+		return $wsr->pressure()->absolute($args['pressureUnit']);
+	}
+
+	public function relativePressure(AggRecord $wsr, array $args): ?float
+	{
+		return $wsr->pressure()->relative($args['pressureUnit']);
+	}
+
+	public function indoorTemperature(AggRecord $wsr, array $args): ?float
+	{
+		return $wsr->temperature()->indoor($args['temperatureUnit']);
+	}
+
+	public function outdoorTemperature(AggRecord $wsr, array $args): ?float
+	{
+		return $wsr->temperature()->outdoor($args['temperatureUnit']);
+	}
+
+	public function indoorHumidity(AggRecord $wsr, array $args): ?float
+	{
+		return $wsr->humidity()->indoor($args['humidityUnit']);
+	}
+
+	public function outdoorHumidity(AggRecord $wsr, array $args): ?float
+	{
+		return $wsr->humidity()->outdoor($args['humidityUnit']);
+	}
+
+	public function windSpeed(AggRecord $wsr, array $args): ?float
+	{
+		return $wsr->wind()->speed($args['windSpeedUnit']);
+	}
+
+	public function windAzimuth(AggRecord $wsr): ?float
+	{
+		return $wsr->wind()->directionAzimuth();
+	}
+
+	public function windGust(AggRecord $wsr, array $args): ?float
+	{
+		return $wsr->wind()->gust($args['windSpeedUnit']);
+	}
+
+}
--- src/Devices/Infrastructure/Delivery/API/GraphQL/WeatherStationRecord.php
+++ src/Devices/Infrastructure/Delivery/API/GraphQL/WeatherStationRecord.php
@@ -2,59 +2,19 @@
 
 namespace Adeira\Connector\Devices\Infrastructure\Delivery\API\GraphQL;
 
-use Adeira\Connector\Devices\DomainModel\WeatherStation\AggregatedWeatherStationRecord as AggRecord;
+use Adeira\Connector\Devices\DomainModel\WeatherStation\WeatherStationRecord as Record;
 
 final class WeatherStationRecord
 {
 
-	public function aggregatedDate(AggRecord $wsr): \DateTimeImmutable
+	public function id(Record $wsr): string
 	{
-		return $wsr->aggregatedDate();
+		return $wsr->id()->toString();
 	}
 
-	public function absolutePressure(AggRecord $wsr, array $args): ?float
+	public function creationDate(Record $wsr): \DateTimeImmutable
 	{
-		return $wsr->pressure()->absolute($args['pressureUnit']);
-	}
-
-	public function relativePressure(AggRecord $wsr, array $args): ?float
-	{
-		return $wsr->pressure()->relative($args['pressureUnit']);
-	}
-
-	public function indoorTemperature(AggRecord $wsr, array $args): ?float
-	{
-		return $wsr->temperature()->indoor($args['temperatureUnit']);
-	}
-
-	public function outdoorTemperature(AggRecord $wsr, array $args): ?float
-	{
-		return $wsr->temperature()->outdoor($args['temperatureUnit']);
-	}
-
-	public function indoorHumidity(AggRecord $wsr, array $args): ?float
-	{
-		return $wsr->humidity()->indoor($args['humidityUnit']);
-	}
-
-	public function outdoorHumidity(AggRecord $wsr, array $args): ?float
-	{
-		return $wsr->humidity()->outdoor($args['humidityUnit']);
-	}
-
-	public function windSpeed(AggRecord $wsr, array $args): ?float
-	{
-		return $wsr->wind()->speed($args['windSpeedUnit']);
-	}
-
-	public function windAzimuth(AggRecord $wsr): ?float
-	{
-		return $wsr->wind()->directionAzimuth();
-	}
-
-	public function windGust(AggRecord $wsr, array $args): ?float
-	{
-		return $wsr->wind()->gust($args['windSpeedUnit']);
+		return $wsr->creationDate();
 	}
 
 }
