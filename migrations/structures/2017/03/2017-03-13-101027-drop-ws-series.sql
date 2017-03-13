21635090f015be879b6d5e6346f76c3f97ab66fa
Martin Zlámal <mrtnzlml@gmail.com>
2017-03-13 10:12:06 +0100

    Remove deprecated WS series

---
+++ migrations/structures/2017/03/2017-03-13-101027-drop-ws-series.sql
@@ -0,0 +1,1 @@
+DROP TABLE weather_stations_series;
--- src/Devices/Infrastructure/Delivery/API/GraphQL/WeatherStationsConnection.php
+++ src/Devices/Infrastructure/Delivery/API/GraphQL/WeatherStationsConnection.php
@@ -3,7 +3,6 @@
 namespace Adeira\Connector\Devices\Infrastructure\Delivery\API\GraphQL;
 
 use Adeira\Connector\Devices\Application\Service\WeatherStation\CountAllWeatherStations;
-use Adeira\Connector\Devices\Application\Service\WeatherStation\ViewAllWeatherStationSeries;
 use Adeira\Connector\Devices\DomainModel\WeatherStation\WeatherStation;
 use Adeira\Connector\GraphQL\Context;
 
@@ -15,15 +14,9 @@ final class WeatherStationsConnection
 	 */
 	private $countAllWeatherStations;
 
-	/**
-	 * @var \Adeira\Connector\Devices\Application\Service\WeatherStation\ViewAllWeatherStationSeries
-	 */
-	private $allWeatherStationsSeries;
-
-	public function __construct(CountAllWeatherStations $countAllWeatherStations, ViewAllWeatherStationSeries $allWeatherStationsSeries)
+	public function __construct(CountAllWeatherStations $countAllWeatherStations)
 	{
 		$this->countAllWeatherStations = $countAllWeatherStations;
-		$this->allWeatherStationsSeries = $allWeatherStationsSeries;
 	}
 
 	/**
@@ -51,15 +44,6 @@ final class WeatherStationsConnection
 	public function weatherStations(array $ws, array $args, Context $context): array
 	{
 		return $ws; // fall through
-	}
-
-	/**
-	 * @param WeatherStation[] $ws
-	 * @return WeatherStationSeries[]
-	 */
-	public function series(array $ws, array $args, Context $context): array
-	{
-		return $this->allWeatherStationsSeries->execute($context->userId());
 	}
 
 }
