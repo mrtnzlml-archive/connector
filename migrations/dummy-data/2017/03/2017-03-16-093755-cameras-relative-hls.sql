a2f40a579041257b0dd4e44c40bb16b579e9cf0a
Martin Zlámal <mrtnzlml@gmail.com>
2017-03-16 09:40:25 +0100

    Cameras: make all HLS streams relative

---
+++ migrations/dummy-data/2017/03/2017-03-16-093755-cameras-relative-hls.sql
@@ -0,0 +1,2 @@
+UPDATE cameras SET stream_hls = '/hls/Y9c7gqdbevAzww7LfJYbJg/stream.m3u8' WHERE id = 'c87c0ea0-c40f-4883-9a8f-e95ff6a66b94';
+UPDATE cameras SET stream_hls = '/hls/ZXqmGvP3sMJceQGkQCWiUY/stream.m3u8' WHERE id = '52335bb2-e7b3-4f90-8c0b-acfe408d8259';
